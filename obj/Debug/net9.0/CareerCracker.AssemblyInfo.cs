82b384e5b60979096a6d1f495cbe03e327b99ce7
yogeshktech <yogeshkumar0700@gmail.com>
2025-12-08 10:54:05 +0530

    update code

--- obj/Debug/net9.0/CareerCracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/CareerCracker.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CareerCracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9541e5607de5a6fc07a187e4fbf90d90f9242f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a91642bb9836047c203d0cdf0bd0960c522674e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CareerCracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CareerCracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
