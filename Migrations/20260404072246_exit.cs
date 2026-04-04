d6e2b998e105399bf986c4fb287caebe42237296
Praveen babu <praveenbabu7300@example.com>
2026-04-04 17:41:31 +0530

    update database

---
+++ Migrations/20260404072246_exit.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CareerCracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class exit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Salary",
+                table: "AspNetUsers",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "experience",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "position",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "profile_image",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "specialization",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Salary",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "experience",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "position",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "profile_image",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "specialization",
+                table: "AspNetUsers");
+        }
+    }
+}
--- obj/Debug/net9.0/CareerCracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/CareerCracker.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CareerCracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5050429eff87f854d77c9b1868200bbfc778745e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca0d69aeaaa20eef0edf41ee3ae5069a8fa67be3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CareerCracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CareerCracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
